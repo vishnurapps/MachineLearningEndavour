8b6f05166c1fa5600bb16abcdbda9b32dd104ecc
Vishnu R <vishnurapps@gmail.com>
2018-03-04 23:18:10 +0530

    Read dataset and extracted dependent and independent variable

--- 01_preprocessing/Preprocessing.py
+++ 01_preprocessing/Preprocessing.py
@@ -10,3 +10,13 @@ Created on Sun Mar  4 22:46:48 2018
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+
+#Importing dataset
+dataset = pd.read_csv('Data.csv')
+'''iloc stands for integer location, which is used for slicing the dataset
+we can see [:, :-1] below which has a comma
+what is on left of the comma is the rows and what is on right is column
+: means we want all rows and :-1 means we need all but last column
+values means we want to take the values'''
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 3].values
