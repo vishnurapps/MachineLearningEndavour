954e64afd35572b60389f63ad82f2610a298bc5b
Vishnu R <vishnurapps@gmail.com>
2018-03-05 23:03:17 +0530

    Finished data preprocessing

---
+++ 02_simple_linear_regression/Simple_Linear_Regression.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  5 22:31:06 2018
+
+@author: vishnu
+"""
+
+"""
+Simple linear regression deals with equations of the form y = b0 + b1x, where b0 is the y intercept 
+and b1 is the slope of the line. Here y is the dependent variable and x is the independent variable
+The best fit line is calculated using the Ordinary Least Squares. Best fit line is the line having the 
+smallest Ordinary Least Squares.
+"""
+#Importing Libraries that are required for preprocessing
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#Importing dataset
+dataset = pd.read_csv('Salary_Data.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 1].values
+
+#Splitting the dataset into training set and test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
+
+"""
+#Feature scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+"""
