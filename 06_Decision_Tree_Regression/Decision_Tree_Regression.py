cf6cfe46094ac7d6fb02d363a6813e9b73880f12
Vishnu R <vishnurapps@gmail.com>
2018-03-11 15:29:13 +0530

    Plotting in high resolution

--- 06_Decision_Tree_Regression/Decision_Tree_Regression.py
+++ 06_Decision_Tree_Regression/Decision_Tree_Regression.py
@@ -53,3 +53,13 @@ plt.title("Truth or Bluff (Decision Tree Regression)")
 plt.xlabel("Position Level")
 plt.ylabel("Salary")
 plt.show()
+
+#Plotting in high resolution
+X_grid = np.arange(min(X), max(X), 0.1)
+X_grid = X_grid.reshape((len(X_grid),1))
+plt.scatter(X, y, color = "red")
+plt.plot(X_grid, regressor.predict(X_grid), color = "blue")
+plt.title("Truth or Bluff (Decision Tree Regression)")
+plt.xlabel("Position Level")
+plt.ylabel("Salary")
+plt.show()
