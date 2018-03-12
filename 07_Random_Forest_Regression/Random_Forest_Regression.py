4e8f10ae456f565048da4f771911e5e99f69b418
Vishnu R <vishnurapps@gmail.com>
2018-03-12 23:50:47 +0530

    Random Forest with 10 trees

--- 07_Random_Forest_Regression/Random_Forest_Regression.py
+++ 07_Random_Forest_Regression/Random_Forest_Regression.py
@@ -30,3 +30,12 @@ from sklearn.ensemble import RandomForestRegressor
 regressor = RandomForestRegressor(n_estimators = 10, random_state = 0)      #n_estimators - Number of decision trees
 regressor.fit(X,y)            
 
+#Plotting with 10 trees
+X_grid = np.arange(min(X), max(X), 0.1)
+X_grid = X_grid.reshape((len(X_grid),1))
+plt.scatter(X, y, color = "red")
+plt.plot(X_grid, regressor.predict(X_grid), color = "blue")
+plt.title("Truth or Bluff (Random Forest Regression)")
+plt.xlabel("Position Level")
+plt.ylabel("Salary")
+plt.show()
