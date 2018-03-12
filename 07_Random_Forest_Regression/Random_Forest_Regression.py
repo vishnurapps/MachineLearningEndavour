c06ce370ba9e4ea629c923f15f4e2d08288c7d60
Vishnu R <vishnurapps@gmail.com>
2018-03-12 23:43:58 +0530

    Simple explanation of Random Forest Regression

---
+++ 07_Random_Forest_Regression/Random_Forest_Regression.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 12 23:33:20 2018
+
+@author: vishnu
+"""
+
+"""
+Random force is a version of ensemble learning. Ensemble learning is when you take multiple algorithms 
+or the same algorithm multiple times and you put them together to make something much more powerful 
+than the original. Out of all the data points we have, we are going to take K data points and 
+we will build a decision tree based on that. In Random Forest Regression, we will plot so many 
+decision trees and to predict the result, we will take the average of the prediction of all these 
+decision trees
+"""
+
+            
+
