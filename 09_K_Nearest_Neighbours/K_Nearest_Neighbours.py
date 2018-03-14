a43a49aac9dc2b21953a6696920a794e9b6a533c
Vishnu R <vishnurapps@gmail.com>
2018-03-14 22:27:20 +0530

    Fitting to K-NN dataset

--- 09_K_Nearest_Neighbours/K_Nearest_Neighbours.py
+++ 09_K_Nearest_Neighbours/K_Nearest_Neighbours.py
@@ -27,9 +27,9 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('Position_Salaries.csv')
-X = dataset.iloc[:, 1:2].values     
-y = dataset.iloc[:, 2].values 
+dataset = pd.read_csv('Social_Network_Ads.csv')
+X = dataset.iloc[:, [2, 3]].values
+y = dataset.iloc[:, 4].values
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.cross_validation import train_test_split
@@ -41,3 +41,7 @@ sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
+#Fitting K-NN to dataset
+from sklearn.neighbors import KNeighborsClassifier
+classifier = KNeighborsClassifier(n_neighbors = 5, metric = "minkowski", p = 2)     #minkowski and p =2 means euclidean distance
+classifier.fit(X_train, y_train)
