f92c93e6a79b1b01fe06c5e128a297590f31c6a8
Vishnu R <vishnurapps@gmail.com>
2018-03-17 23:30:20 +0530

    Fitting Naive Bayes to dataset

--- 12_Naive_Bayes/Naive_Bayes.py
+++ 12_Naive_Bayes/Naive_Bayes.py
@@ -26,3 +26,8 @@ from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
+
+#Fitting Naive Bayes to dataset
+from sklearn.naive_bayes import GaussianNB
+classifier = GaussianNB()
+classifier.fit(X_train, y_train)
