4ee8c9c0d6d2e0e8a9e0d1e69ebcfa34b5764a87
Vishnu R <vishnurapps@gmail.com>
2018-03-19 23:40:24 +0530

    Fitting Decision Tree Classification to dataset

--- 13_Decision_Tree_Classification/Decision_Tree_Classification.py
+++ 13_Decision_Tree_Classification/Decision_Tree_Classification.py
@@ -25,3 +25,8 @@ from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
+
+#Fitting Decision Tree Classification to dataset
+from sklearn.tree import DecisionTreeClassifier
+classifier = DecisionTreeClassifier(criterion = "entropy", random_state = 0)
+classifier.fit(X_train, y_test)
