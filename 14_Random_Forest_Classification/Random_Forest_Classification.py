60e770ade8954658087b4cb512c6e86939e9370c
Vishnu R <vishnurapps@gmail.com>
2018-03-26 23:49:34 +0530

    Importing libraries and dataset

---
+++ 14_Random_Forest_Classification/Random_Forest_Classification.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 26 23:47:43 2018
+
+@author: vishnu
+"""
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Social_Network_Ads.csv')
+X = dataset.iloc[:, [2, 3]].values
+y = dataset.iloc[:, 4].values
