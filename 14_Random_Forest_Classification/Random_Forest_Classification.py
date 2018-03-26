3d98aa703c696fe1c9f73050813e0f3c13efb831
Vishnu R <vishnurapps@gmail.com>
2018-03-26 23:54:03 +0530

    Fitting Random Forest Classification to dataset

--- 14_Random_Forest_Classification/Random_Forest_Classification.py
+++ 14_Random_Forest_Classification/Random_Forest_Classification.py
@@ -25,3 +25,8 @@ from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
+
+#Fitting Random Forest Classification to dataset
+from sklearn.ensemble import RandomForestClassifier
+classifier = RandomForestClassifier(n_estimators = 10, criterion = "entropy", random_state = 0)
+classifier.fit(X_train, y_train)
