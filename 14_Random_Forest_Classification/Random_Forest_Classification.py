bb82853e09bbca4abe5dbb8e58553e3dce874fd6
Vishnu R <vishnurapps@gmail.com>
2018-03-26 23:54:42 +0530

    Confusion Matrix

--- 14_Random_Forest_Classification/Random_Forest_Classification.py
+++ 14_Random_Forest_Classification/Random_Forest_Classification.py
@@ -30,3 +30,10 @@ X_test = sc.transform(X_test)
 from sklearn.ensemble import RandomForestClassifier
 classifier = RandomForestClassifier(n_estimators = 10, criterion = "entropy", random_state = 0)
 classifier.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
