5a1467f9d69c341eb65a961b78d3bc767061823c
Vishnu R <vishnurapps@gmail.com>
2018-04-03 23:17:51 +0530

    Elbow meathod for finding the optimal number of clusters

--- 15_K_Means/K_Means.py
+++ 15_K_Means/K_Means.py
@@ -14,3 +14,18 @@ import pandas as pd
 # Importing the dataset
 dataset = pd.read_csv('Mall_Customers.csv')
 X = dataset.iloc[:, [3,4]].values
+
+#Elbow meathod for finding the optimal number of clusters
+from sklearn.cluster import KMeans
+wcss = []               #Within Cluster Sum of Squares
+for i in range(1, 11):
+    kmeans = KMeans(n_clusters = i, init = 'k-means++', random_state = 42)
+    kmeans.fit(X)
+    wcss.append(kmeans.inertia_)
+plt.plot(range(1, 11), wcss)
+plt.title('The Elbow Method')
+plt.xlabel('Number of clusters')
+plt.ylabel('WCSS')
+plt.show()
+
+
