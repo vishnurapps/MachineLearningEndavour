78a6c685e5c30b4d6f7189c99f80ddb8449076a1
Vishnu R <vishnurapps@gmail.com>
2018-04-05 00:49:14 +0530

    Importing libraries and Dataset

---
+++ 17_Apriori/Apriori.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  5 00:39:12 2018
+
+@author: vishnu
+"""
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Market_Basket_Optimisation.csv', header = None)
