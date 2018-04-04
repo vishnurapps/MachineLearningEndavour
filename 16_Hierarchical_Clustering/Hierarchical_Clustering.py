0839c06bf7110bcf56581c84334982a3b7ef4d45
Vishnu R <vishnurapps@gmail.com>
2018-04-04 22:27:18 +0530

    Finding the optimal number of clusters using dentogram

--- 16_Hierarchical_Clustering/Hierarchical_Clustering.py
+++ 16_Hierarchical_Clustering/Hierarchical_Clustering.py
@@ -14,3 +14,11 @@ import pandas as pd
 # Importing the dataset
 dataset = pd.read_csv('Mall_Customers.csv')
 X = dataset.iloc[:, [3,4]].values
+
+# Finding the optimal number of clusters using dentogram
+import scipy.cluster.hierarchy as sch
+dentogram = sch.dendrogram(sch.linkage(X, method = "ward"))
+plt.title("Dentogram")
+plt.xlabel("Customers")
+plt.ylabel("Euclidean Distances")
+plt.show()
