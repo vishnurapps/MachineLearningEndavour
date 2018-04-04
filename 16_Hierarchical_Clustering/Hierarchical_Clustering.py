dcabf3e808b25ee16bece3b8ba97ee721c1f9e83
Vishnu R <vishnurapps@gmail.com>
2018-04-04 22:38:34 +0530

    Plotting the customers

--- 16_Hierarchical_Clustering/Hierarchical_Clustering.py
+++ 16_Hierarchical_Clustering/Hierarchical_Clustering.py
@@ -27,3 +27,15 @@ plt.show()
 from sklearn.cluster import AgglomerativeClustering
 hc = AgglomerativeClustering(n_clusters = 5, affinity = "euclidean", linkage = "ward")
 y_hc = hc.fit_predict(X)
+
+# Visualising the clusters
+plt.scatter(X[y_hc == 0, 0], X[y_hc == 0, 1], s = 25, c = 'red', label = 'Careful')
+plt.scatter(X[y_hc == 1, 0], X[y_hc == 1, 1], s = 25, c = 'blue', label = 'Standard')
+plt.scatter(X[y_hc == 2, 0], X[y_hc == 2, 1], s = 25, c = 'green', label = 'Target')
+plt.scatter(X[y_hc == 3, 0], X[y_hc == 3, 1], s = 25, c = 'cyan', label = 'Careless')
+plt.scatter(X[y_hc == 4, 0], X[y_hc == 4, 1], s = 25, c = 'magenta', label = 'Sensible ')
+plt.title('Clusters of customers')
+plt.xlabel('Annual Income (k$)')
+plt.ylabel('Spending Score (1-100)')
+plt.legend()
+plt.show()
