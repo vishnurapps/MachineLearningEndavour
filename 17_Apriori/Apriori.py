f35160f5bae6becdea0222d5eefa55855baab747
Vishnu R <vishnurapps@gmail.com>
2018-04-05 08:42:43 +0530

    Visualising the results

--- 17_Apriori/Apriori.py
+++ 17_Apriori/Apriori.py
@@ -22,3 +22,6 @@ for i in range(0, 7501):
 # Training Apriori on dataset
 from apyori import apriori
 rules = apriori(transactions, min_support = 0.003, min_confidence = 0.2, min_lift = 3, min_length = 2)
+
+# Visualising the results
+results = list(rules)
